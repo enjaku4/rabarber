0883935ab4836bc82025f44dd8f777eb922328d3
enjaku4 <enjaku4@gmail.com>
2023-02-25 12:38:14 +0200

    remove failing test

--- spec/rabarber_spec.rb
+++ spec/rabarber_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Rabarber do
   it "has a version number" do
     expect(Rabarber::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end

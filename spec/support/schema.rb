f2cc6ef2311cea7854ddd4a21ae7cd8bfab6d3b1
enjaku4 <enjaku4@gmail.com>
2023-12-29 19:09:15 +0100

    add missing foreign keys to schema

--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -22,4 +22,7 @@ ActiveRecord::Schema.define do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "rabarber_roles_roleables", "rabarber_roles", column: "role_id"
+  add_foreign_key "rabarber_roles_roleables", "users", column: "roleable_id"
 end

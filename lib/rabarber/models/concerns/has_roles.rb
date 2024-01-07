47a6dad3839642303580d85669af913808306c8a
enjaku4 <enjaku4@gmail.com>
2024-01-07 02:05:44 +0100

    add RoleNames module and allow Strings in HasRoles methods

--- lib/rabarber/models/concerns/has_roles.rb
+++ lib/rabarber/models/concerns/has_roles.rb
@@ -18,39 +18,23 @@ module Rabarber
       rabarber_roles.names
     end
 
-    # TODO
     def has_role?(*role_names)
-      validate_role_names(role_names)
-
-      (roles & role_names).any?
+      (roles & RoleNames.pre_process(role_names)).any?
     end
 
-    # TODO
     def assign_roles(*role_names, create_new: true)
-      validate_role_names(role_names)
+      roles_to_assign = RoleNames.pre_process(role_names)
 
-      create_new_roles(role_names) if create_new
+      create_new_roles(roles_to_assign) if create_new
 
-      rabarber_roles << Role.where(name: role_names) - rabarber_roles
+      rabarber_roles << Role.where(name: roles_to_assign) - rabarber_roles
     end
 
-    # TODO
     def revoke_roles(*role_names)
-      validate_role_names(role_names)
-
-      self.rabarber_roles = rabarber_roles - Role.where(name: role_names)
+      self.rabarber_roles = rabarber_roles - Role.where(name: RoleNames.pre_process(role_names))
     end
 
     private
-
-    def validate_role_names(role_names)
-      return if role_names.all? { |role_name| role_name.is_a?(Symbol) && role_name.to_s.match?(Role::NAME_REGEX) }
-
-      raise(
-        InvalidArgumentError,
-        "Role names must be Symbols and may only contain lowercase letters, numbers and underscores"
-      )
-    end
 
     def create_new_roles(role_names)
       new_roles = role_names - Role.names
--- lib/rabarber/models/role.rb
+++ lib/rabarber/models/role.rb
@@ -4,9 +4,7 @@ module Rabarber
   class Role < ActiveRecord::Base
     self.table_name = "rabarber_roles"
 
-    NAME_REGEX = /\A[a-z0-9_]+\z/
-
-    validates :name, presence: true, uniqueness: true, format: { with: NAME_REGEX }
+    validates :name, presence: true, uniqueness: true, format: { with: RoleNames::REGEX }
 
     def self.names
       pluck(:name).map(&:to_sym)

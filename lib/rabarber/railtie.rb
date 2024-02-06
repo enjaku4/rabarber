03978beea7486e8ae6496fce0635189c0fab10cd
enjaku4 <enjaku4@gmail.com>
2024-02-06 21:54:07 +0100

    prevent role checking if roles rable doesn't exist

--- lib/rabarber/railtie.rb
+++ lib/rabarber/railtie.rb
@@ -7,7 +7,7 @@ module Rabarber
     initializer "rabarber.after_initialize" do |app|
       app.config.after_initialize do
         Rabarber::Missing::Actions.new.handle
-        Rabarber::Missing::Roles.new.handle
+        Rabarber::Missing::Roles.new.handle if Rabarber::Role.table_exists?
       end
     end
   end

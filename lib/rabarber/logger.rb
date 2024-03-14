8a4491ac8a93898e862d98cdf59774fc1f7812b0
enjaku4 <enjaku4@gmail.com>
2024-03-14 20:33:25 +0100

    add todo

--- lib/rabarber/logger.rb
+++ lib/rabarber/logger.rb
@@ -11,6 +11,7 @@ module Rabarber
     def audit(log_level, message)
       return unless Rabarber::Configuration.instance.audit_trail_enabled
 
+      # TODO: Logger must not be initialized every time this method is called
       ::Logger.new(Rails.root.join("log/rabarber_audit.log")).public_send(log_level, message)
     end
   end

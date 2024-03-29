e0b53545e9f85d1a328af37a45fff9f77c3c2f1b
enjaku4 <enjaku4@gmail.com>
2024-03-29 01:47:51 +0100

    railtie spec

--- spec/rabarber/railtie_spec.rb
+++ spec/rabarber/railtie_spec.rb
@@ -4,8 +4,12 @@ RSpec.describe Rabarber::Railtie do
   subject { described_class.initializers.detect { |i| i.name == "rabarber.after_initialize" }.run(DummyApplication) }
 
   context "when eager_load is true" do
-    it "checks the actions" do
-      expect(Rabarber::Missing::Actions).to receive_message_chain(:new, :handle)
+    let(:double) { instance_double(Rabarber::Core::PermissionsIntegrityChecker) }
+
+    before { allow(Rabarber::Core::PermissionsIntegrityChecker).to receive(:new).with(no_args).and_return(double) }
+
+    it "checks the integrity" do
+      expect(double).to receive(:run)
       subject
     end
   end
@@ -14,7 +18,7 @@ RSpec.describe Rabarber::Railtie do
     before { allow(Rails.configuration).to receive(:eager_load).and_return(false) }
 
     it "does not check the actions" do
-      expect(Rabarber::Missing::Actions).not_to receive(:new)
+      expect_any_instance_of(Rabarber::Core::PermissionsIntegrityChecker).not_to receive(:run)
       subject
     end
   end

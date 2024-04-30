ab9d8f17da8b43346b121fb03f5e044d4ac4e4fd
enjaku4 <enjaku4@gmail.com>
2024-05-01 00:45:48 +0200

    update schema

--- spec/support/schema.rb
+++ spec/support/schema.rb
@@ -5,8 +5,11 @@ ActiveRecord::Schema.define do
 
   create_table "rabarber_roles", force: :cascade do |t|
     t.string "name", null: false
+    t.string "context_type"
+    t.integer "context_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["context_type", "context_id"], name: "index_rabarber_roles_on_context"
     t.index ["name"], name: "index_rabarber_roles_on_name", unique: true
   end
 

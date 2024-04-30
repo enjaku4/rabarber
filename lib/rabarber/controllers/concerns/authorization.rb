bc7a2c44ba5cdf547f52625349439bcb51da884d
enjaku4 <enjaku4@gmail.com>
2024-04-30 19:59:19 +0200

    todos

--- lib/rabarber/controllers/concerns/authorization.rb
+++ lib/rabarber/controllers/concerns/authorization.rb
@@ -12,6 +12,14 @@ module Rabarber
 
     class_methods do
       def grant_access(action: nil, roles: nil, if: nil, unless: nil)
+        # TODO: this must accept context argument which is nil by default, but can also be a class or an instance
+        # TODO: the context stored in memory must be represented as a hash { class_name: ..., id: ... }
+        # TODO: both values in the hash can be nil
+        # TODO: when access permissions are checked, the roles must be checked for the context
+        # TODO: class_name.nil? && id.nil? means checking for global roles
+        # TODO: class_name.nil? && !id.nil? means checking for class roles (e.g. a user can be an admin of every Project)
+        # TODO: !class_name.nil? && !id.nil? means checking for instance roles
+        # TODO: default nil context should provide a seamless upgrade as previous versions supported only global roles
         dynamic_rule, negated_dynamic_rule = binding.local_variable_get(:if), binding.local_variable_get(:unless)
 
         Rabarber::Core::Permissions.add(

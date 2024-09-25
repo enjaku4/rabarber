201ddd6a70d2cd55ace8acbd8c1c160711a16f0a
enjaku4 <enjaku4@gmail.com>
2024-09-25 23:34:17 +0200

    optimize view helpers

--- lib/rabarber/helpers/helpers.rb
+++ lib/rabarber/helpers/helpers.rb
@@ -5,15 +5,19 @@ module Rabarber
     include Rabarber::Core::Roleable
 
     def visible_to(*roles, context: nil, &block)
-      return if roleable_roles(context: Rabarber::Input::Context.new(context).process).intersection(Rabarber::Input::Roles.new(roles).process).none?
-
-      capture(&block)
+      capture(&block) if roles_match?(roles, context)
     end
 
     def hidden_from(*roles, context: nil, &block)
-      return if roleable_roles(context: Rabarber::Input::Context.new(context).process).intersection(Rabarber::Input::Roles.new(roles).process).any?
+      capture(&block) unless roles_match?(roles, context)
+    end
 
-      capture(&block)
+    private
+
+    def roles_match?(roles, context)
+      processed_roles = Rabarber::Input::Roles.new(roles).process
+      processed_context = Rabarber::Input::Context.new(context).process
+      roleable_roles(context: processed_context).any? { |role| processed_roles.include?(role) }
     end
   end
 end

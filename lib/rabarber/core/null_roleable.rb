e7cc63ee71bcf7cfce6e838e2fa95d1a333f95a7
enjaku4 <enjaku4@gmail.com>
2024-10-02 21:32:48 +0200

    remove duplicated code

--- lib/rabarber/core/null_roleable.rb
+++ lib/rabarber/core/null_roleable.rb
@@ -2,16 +2,6 @@
 
 module Rabarber
   module Core
-    module Roleable
-      def roleable
-        send(Rabarber::Configuration.instance.current_user_method) || NullRoleable.new
-      end
-
-      def roleable_roles(context: nil)
-        roleable.roles(context: context)
-      end
-    end
-
     class NullRoleable
       def roles(context:) # rubocop:disable Lint/UnusedMethodArgument
         []

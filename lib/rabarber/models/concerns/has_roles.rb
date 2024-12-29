fb7036066a09bf126b4bad87d2d9d829002d4a42
enjaku4 <enjaku4@gmail.com>
2024-12-29 13:13:19 +0100

    check db roles instead of cached roles

--- lib/rabarber/models/concerns/has_roles.rb
+++ lib/rabarber/models/concerns/has_roles.rb
@@ -59,7 +59,7 @@ module Rabarber
       processed_context = process_context(context)
 
       roles_to_revoke = Rabarber::Role.where(
-        name: processed_role_names.intersection(roles(context: processed_context)), **processed_context
+        name: processed_role_names.intersection(rabarber_roles.names(context: processed_context)), **processed_context
       )
 
       if roles_to_revoke.any?

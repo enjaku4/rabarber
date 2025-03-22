a6076cc951ab8f6d67bd041ab4dbba61ba6c7bd9
enjaku4 <enjaku4@gmail.com>
2025-03-22 09:03:17 +0100

    rubocop

--- lib/rabarber/helpers/helpers.rb
+++ lib/rabarber/helpers/helpers.rb
@@ -4,12 +4,12 @@ module Rabarber
   module Helpers
     include Rabarber::Core::Roleable
 
-    def visible_to(*roles, context: nil, &block)
-      capture(&block) if roleable.has_role?(*roles, context:)
+    def visible_to(*roles, context: nil, &)
+      capture(&) if roleable.has_role?(*roles, context:)
     end
 
-    def hidden_from(*roles, context: nil, &block)
-      capture(&block) unless roleable.has_role?(*roles, context:)
+    def hidden_from(*roles, context: nil, &)
+      capture(&) unless roleable.has_role?(*roles, context:)
     end
   end
 end

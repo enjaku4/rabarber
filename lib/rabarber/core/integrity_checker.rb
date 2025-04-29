5ab230112ca93687ba2ea24556947d6d9c7d6182
enjaku4 <enjaku4@gmail.com>
2025-04-29 03:05:33 +0200

    check for missing context classes

--- lib/rabarber/core/integrity_checker.rb
+++ lib/rabarber/core/integrity_checker.rb
@@ -10,20 +10,33 @@ module Rabarber
       end
 
       def run!
-        # TODO: raise if class context is missing in any role
-        # TODO: prune roles with missing instance context
-        return if missing_list.empty?
-
-        raise(
-          Rabarber::Error,
-          "Following actions were passed to 'grant_access' method but are not defined in the controller:\n#{missing_list.to_yaml}"
-        )
+        check_for_missing_context_class
+        check_for_missing_actions
+        prune_missing_instance_context_roles
       end
 
       private
 
-      def missing_list
-        @missing_list ||= action_rules.each_with_object([]) do |(controller, hash), arr|
+      def check_for_missing_context_class
+        # TODO: tests
+        Rabarber::Role.distinct.pluck(:context_type).each do |context_class|
+          context_class.constantize
+        rescue NameError => e
+          raise Rabarber::Error, "Context not found: class #{e.name} was renamed or deleted"
+        end
+      end
+
+      def check_for_missing_actions
+        return if missing_actions_list.empty?
+
+        raise(
+          Rabarber::Error,
+          "Following actions were passed to 'grant_access' method but are not defined in the controller:\n#{missing_actions_list.to_yaml}"
+        )
+      end
+
+      def missing_actions_list
+        @missing_actions_list ||= action_rules.each_with_object([]) do |(controller, hash), arr|
           missing_actions = hash.keys - controller.action_methods.map(&:to_sym)
           arr << { controller => missing_actions } if missing_actions.any?
         end
@@ -32,6 +45,11 @@ module Rabarber
       def action_rules
         rules = Rabarber::Core::Permissions.action_rules
         controller ? rules.slice(controller) : rules
+      end
+
+      def prune_missing_instance_context_roles
+        # TODO: tests
+        # TODO: implement
       end
     end
   end

e84d4d5158cc72fc8afcd0c45b00d8de1d3cd0a2
enjaku4 <enjaku4@gmail.com>
2025-05-03 01:06:40 +0200

    fix railtie

--- lib/rabarber/railtie.rb
+++ lib/rabarber/railtie.rb
@@ -9,6 +9,11 @@ module Rabarber
         Rabarber::Core::Permissions.reset! unless app.config.eager_load
         user_model = Rabarber::Configuration.instance.user_model
         user_model.include Rabarber::HasRoles unless user_model < Rabarber::HasRoles
+      end
+    end
+
+    initializer "rabarber.after_initialize" do |app|
+      app.config.after_initialize do
         Rabarber::Core::IntegrityChecker.run! if app.config.eager_load
       end
     end
--- spec/rabarber/railtie_spec.rb
+++ spec/rabarber/railtie_spec.rb
@@ -1,16 +1,53 @@
 # frozen_string_literal: true
 
 RSpec.describe Rabarber::Railtie do
-  subject { DummyApplication.config.to_prepare_blocks.each(&:call) }
+  context "to_prepare" do
+    subject { DummyApplication.config.to_prepare_blocks.each(&:call) }
 
-  it "checks the integrity and includes the Rabarber::HasRoles module" do
-    expect(Rabarber::Core::IntegrityChecker).to receive(:run!)
-    subject
-    expect(User < Rabarber::HasRoles).to be true
+    context "when eager_load is true" do
+      it "includes the Rabarber::HasRoles module" do
+        expect(User < Rabarber::HasRoles).to be true
+      end
+
+      it "does not reset permissions" do
+        expect(Rabarber::Core::Permissions).not_to receive(:reset!)
+        subject
+      end
+    end
+
+    context "when eager_load is false" do
+      before { allow(Rails.configuration).to receive(:eager_load).and_return(false) }
+
+      it "includes the Rabarber::HasRoles module" do
+        expect(User < Rabarber::HasRoles).to be true
+      end
+
+      it "resets permissions" do
+        expect(Rabarber::Core::Permissions).to receive(:reset!)
+        subject
+      end
+    end
   end
 
-  it "does not reset permissions" do
-    expect(Rabarber::Core::Permissions).not_to receive(:reset!)
-    subject
+  context "after_initialize" do
+    subject { initializer.run(DummyApplication) }
+
+    let(:initializer) { described_class.initializers.detect { |i| i.name == "rabarber.after_initialize" } }
+
+    context "when eager_load is true" do
+      it "runs the Rabarber::Core::IntegrityChecker" do
+        expect(Rabarber::Core::IntegrityChecker).to receive(:run!)
+        subject
+      end
+    end
+
+    context "when eager_load is false" do
+      before { allow(Rails.configuration).to receive(:eager_load).and_return(false) }
+
+      it "does not run the Rabarber::Core::IntegrityChecker" do
+        expect(Rabarber::Core::IntegrityChecker).not_to receive(:run!)
+        subject
+      end
+    end
   end
 end

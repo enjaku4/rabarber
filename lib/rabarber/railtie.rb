fc7f8d729b141665e9f46f8cbdd91c94db9ec987
enjaku4 <enjaku4@gmail.com>
2025-09-12 22:34:10 +0200

    skip context class verification if roles table is missing

--- lib/rabarber/railtie.rb
+++ lib/rabarber/railtie.rb
@@ -6,10 +6,12 @@ module Rabarber
   class Railtie < Rails::Railtie
     initializer "rabarber.to_prepare" do |app|
       app.config.to_prepare do
-        Rabarber::Role.where.not(context_type: nil).distinct.pluck(:context_type).each do |context_class|
-          context_class.constantize
-        rescue NameError => e
-          raise Rabarber::Error, "Context not found: class `#{e.name}` may have been renamed or deleted"
+        if ActiveRecord::Base.connection.data_source_exists?("rabarber_roles")
+          Rabarber::Role.where.not(context_type: nil).distinct.pluck(:context_type).each do |context_class|
+            context_class.constantize
+          rescue NameError => e
+            raise Rabarber::Error, "Context not found: class `#{e.name}` may have been renamed or deleted"
+          end
         end
 
         Rabarber::Core::Permissions.reset! unless app.config.eager_load

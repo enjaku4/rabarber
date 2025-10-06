f428345d8b910aaf004c67132949519fcaceb319
enjaku4 <enjaku4@gmail.com>
2025-10-06 16:16:39 +0200

    skip db checks when db doesn't exist or connection isn't established

--- lib/rabarber/railtie.rb
+++ lib/rabarber/railtie.rb
@@ -6,13 +6,16 @@ module Rabarber
   class Railtie < Rails::Railtie
     initializer "rabarber.to_prepare" do |app|
       app.config.to_prepare do
-        connection = ActiveRecord::Base.connection
-        if connection.database_exists? && connection.data_source_exists?("rabarber_roles")
-          Rabarber::Role.where.not(context_type: nil).distinct.pluck(:context_type).each do |context_class|
-            context_class.constantize
-          rescue NameError => e
-            raise Rabarber::Error, "Context not found: class `#{e.name}` may have been renamed or deleted"
+        ActiveRecord::Base.with_connection do |connection|
+          if connection.data_source_exists?("rabarber_roles")
+            Rabarber::Role.where.not(context_type: nil).distinct.pluck(:context_type).each do |context_class|
+              context_class.constantize
+            rescue NameError => e
+              raise Rabarber::Error, "Context not found: class `#{e.name}` may have been renamed or deleted"
+            end
           end
+        rescue ActiveRecord::NoDatabaseError, ActiveRecord::ConnectionNotEstablished
+          # Database does not exist yet or connection not established, skip
         end
 
         Rabarber::Core::Permissions.reset! unless app.config.eager_load
